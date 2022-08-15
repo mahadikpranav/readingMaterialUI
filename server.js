81981530f862727f52328a3290ced277c21e536d
pranavmahadik <pranavmahadik50@gmail.com>
2022-08-15 23:12:02 +0530

    dist

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/readingMaterialView'));
+app.use(express.static('./dist/reading-material-view'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/readingMaterialView/'}),
+    res.sendFile('index.html', {root: 'dist/reading-material-view/'}),
 );
 
 // Start the app by listening on the default Heroku port
